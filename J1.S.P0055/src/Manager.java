5bcb029a9082999f748a241b64b96eca3053eda2
hachan140 <106028509+hachan140@users.noreply.github.com>
2022-05-22 23:57:14 +0700

    Add files via upload

---
+++ J1.S.P0055/src/Manager.java
@@ -0,0 +1,104 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Admin
+ */
+public class Manager {
+
+    static int menu() {
+        System.out.println("========= Doctor Management =========");
+        System.out.println("   1. Add Doctor");
+        System.out.println("   2. Update Doctor");
+        System.out.println("   3. Delete Doctor");
+        System.out.println("   4. Search Doctor");
+        System.out.println("   5. Exit");
+        int choice = Validation.checkChoice(1, 5);
+        return choice;
+    }
+
+    static void addDoctor(List<Doctor> ld) {
+        System.out.println("--------- Add Doctor ---------");
+
+        String code = Validation.checkString("Enter Code: ", "[D|d][o][c]\\d++");
+        if (!Validation.checkCodeExist(ld, code)) {
+            System.out.println("Code exist");
+            return;
+        }
+
+        String name = Validation.checkString("Enter Name: ", "[A-Za-z][a-z]+(\\s[A-Za-z][a-z]+){0,3}");
+        String special = Validation.checkString("Enter Specialization: ", "[A-Za-z]");
+        int aval = Validation.checkInt("Enter Availability: ");
+        ld.add(new Doctor(code, name, special, aval));
+
+    }
+
+    static void updateDoctor(List<Doctor> ld) {
+        System.out.println("--------- Update Doctor ---------");
+
+        String code = Validation.checkString("Enter Code: ", "[D|d][o][c]\\d++");
+        if (Validation.checkCodeExist(ld, code)) {
+            System.out.println("Doctor does not exist!");
+            return;
+        }
+        Doctor d = getDoctorByCode(ld, code);
+
+        String codee = Validation.checkString("Enter Code: ", "[D|d][o][c]\\d++");
+        if (!Validation.checkCodeExist(ld, codee)) {
+            System.out.println("Code existed!");
+            return;
+        }
+
+        String name = Validation.checkString("Enter Name: ", "[A-Za-z][a-z]+(\\s[A-Za-z][a-z]+){0,3}");
+
+        String special = Validation.checkString("Enter Specialization: ", "[A-Za-z]");
+
+        int aval = Validation.checkInt("Enter Availability: ");
+        d.setCode(codee);
+        d.setName(name);
+        d.setSpecialization(special);
+        d.setAvailability(aval);
+
+    }
+
+    static Doctor getDoctorByCode(List<Doctor> l, String code) {
+        for (Doctor d : l) {
+            if (d.getCode().equalsIgnoreCase(code)) {
+                return d;
+            }
+        }
+        return null;
+    }
+
+    static void deleteDoctor(List<Doctor> l) {
+        System.out.println("------- Delete Doctor -------");
+        String code = Validation.checkString("Enter Code: ", "[D|d][o][c]\\d++");
+        if (Validation.checkCodeExist(l, code)) {
+            System.out.println("Doctor does not exist!");
+            return;
+        }
+        Doctor d = getDoctorByCode(l, code);
+        l.remove(d);
+    }
+
+    static void searchDoctor(List<Doctor> l) {
+        System.out.println("---------- Search Doctor ----------");
+
+        String name = Validation.checkString("Enter text: ", "[A-Za-z][a-z]+(\\s[A-Za-z][a-z]+){0,3}");
+        System.out.println("--------- Result ------------------");
+        System.out.printf("%-10s%-15s%-25s%-20s\n", "Code", "Name", "Specialization", "Availability");
+        for (Doctor doctor : l) {
+            if (doctor.getName().equalsIgnoreCase(name)) {
+                System.out.printf("%-10s%-15s%-25s%-20d\n", doctor.getCode(),
+                        doctor.getName(), doctor.getSpecialization(), doctor.getAvailability());
+            }
+        }
+    }
+}
---
+++ J1.S.P0055/src/Validation.java
@@ -0,0 +1,68 @@
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Admin
+ */
+public class Validation {
+
+    static Scanner sc = new Scanner(System.in);
+
+    static int checkChoice(int start, int end) {
+        int n;
+        while (true) {
+            try {
+                n = Integer.parseInt(sc.nextLine());
+                if (n >= start && n <= end) {
+                    break;
+                }
+                    else throw new Exception("Error!");
+                } catch(Exception e){
+                    System.out.println("Error, re-enter!");
+                
+            }
+
+        }
+        return n;
+    }
+    static String checkString(String msg, String regex){
+        String word;
+        while(true){
+            System.out.print(msg);
+            word = sc.nextLine();
+            if(word.matches(regex))
+                return word;
+        }
+    }
+     static boolean checkCodeExist(List<Doctor> ld, String code) {
+        for (Doctor doctor : ld) {
+            if (code.equalsIgnoreCase(doctor.getCode())) {
+                return false;
+            }
+        }
+        return true;
+}
+    static int checkInt(String msg){
+        int n;
+        while(true){
+            try{
+                System.out.print(msg);
+                n = Integer.parseInt(sc.nextLine());
+                if(n < 0)
+                    System.out.println("Out of range!");
+                else break;
+            }catch(Exception e){
+                System.out.println("Please input a number!");
+            }
+        }
+        return n;
+}
+}
